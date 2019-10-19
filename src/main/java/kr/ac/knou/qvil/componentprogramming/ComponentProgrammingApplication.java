26b3d4cc9639c37e2313d56c7d7a2a154c286ffb
현태수 <hyeontaesu@hyeontaesuui-MacBookPro.local>
2019-10-19 15:21:50 +0900

    Initialize with Spring Initalizr

---
+++ src/main/java/kr/ac/knou/qvil/componentprogramming/ComponentProgrammingApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.knou.qvil.componentprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponentProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/knou/qvil/componentprogramming/ComponentProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.knou.qvil.componentprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponentProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
